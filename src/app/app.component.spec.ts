e6383da01e237a843dd7b76cbefbdbd3f09cf5b7
Miguel Leon <miguel.e.leon.f@gmail.com>
2021-02-16 00:59:43 -0300

    ✅ update app tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			declarations: [
-				AppComponent
-			],
-		}).compileComponents();
-	});
+
+	beforeEach(() => TestBed.configureTestingModule({
+		imports: [
+			HttpClientTestingModule
+		],
+		declarations: [
+			AppComponent
+		]
+	}).compileComponents());
 
 	it('should create the app', () => {
 		const fixture = TestBed.createComponent(AppComponent);
@@ -16,16 +20,18 @@ describe('AppComponent', () => {
 		expect(app).toBeTruthy();
 	});
 
-	it(`should have as title 'extend-interceptors'`, () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.componentInstance;
-		expect(app.title).toEqual('extend-interceptors');
-	});
-
-	it('should render title', () => {
+	it(`should request and render app title`, () => {
+		const httpTestingController = TestBed.inject(HttpTestingController);
 		const fixture = TestBed.createComponent(AppComponent);
 		fixture.detectChanges();
+
+		const req = httpTestingController.expectOne('title.txt');
+		expect(req.request.method).toEqual('GET');
+		req.flush('testing title');
+		httpTestingController.verify();
+
+		fixture.detectChanges();
 		const compiled = fixture.nativeElement;
-		expect(compiled.querySelector('.content span').textContent).toContain('extend-interceptors app is running!');
+		expect(compiled.querySelector('h1').textContent).toContain('testing title');
 	});
 });
